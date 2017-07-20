769d6fc64d1fcffcbdbaaeafb9bd4d211aed3518
Djalma Araujo <djalma@close.io>
2017-07-20 12:51:32 -0300

    poop updates

--- run-me.sh
+++ run-me.sh
@@ -1,131 +1,135 @@
-# Show hidden files
+printf "################ Setting up things ################\n\n"
+
+printf "Show hidden files\n"
 defaults write com.apple.finder AppleShowAllFiles YES
-# Enable key repeat
+printf "Enable key repeat\n"
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-# Remove all stupid Dock application
+printf "Remove all stupid Dock application\n"
 defaults write com.apple.dock static-only -bool TRUE; killall Dock
 
-# Auto hide Dock
+printf "Auto hide Dock\n"
 defaults write com.apple.dock autohide -bool true && defaults write com.apple.dock autohide-delay -float 0 && defaults write com.apple.dock autohide-time-modifier -float 0.8 && killall Dock
 
-# Dock
+printf "\n\nDock\n\n"
 
-# Dock: enable highlight hover effect for the grid view of a stack
+printf "Dock: enable highlight hover effect for the grid view of a stack\n"
 defaults write com.apple.dock mouse-over-hilite-stack -bool true
 
-# Set the icon size of Dock items to 48 pixels
+printf "Set the icon size of Dock items to 48 pixels\n"
 defaults write com.apple.dock tilesize -int 48
 
-# Dock: enable magnification
+printf "Dock: enable magnification\n"
 defaults write com.apple.dock magnification -bool false
 
-# Set magnification icon size to 80 pixels
+printf "Set magnification icon size to 80 pixels\n"
 defaults write com.apple.dock largesize -float 80
 
-# Dock: minimize windows into their application's icon
+printf "Dock: minimize windows into their applications icon\n"
 defaults write com.apple.dock minimize-to-application -bool true
 
-# Dock: enable spring loading for all Dock items
+printf "Dock: enable spring loading for all Dock items\n"
 defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
 
-# Dock: enable animation when opening applications
+printf "Dock: enable animation when opening applications\n"
 defaults write com.apple.dock launchanim -bool false
 
-# Speed up Mission Control animations
+printf "Speed up Mission Control animations\n"
 defaults write com.apple.dock expose-animation-duration -float 0.1
 
-# General: enable Dashboard as an overlay
+printf "General: enable Dashboard as an overlay\n"
 defaults write com.apple.dock dashboard-in-overlay -bool false
 
-# General: automatically rearrange Spaces based on most recent use
+printf "General: automatically rearrange Spaces based on most recent use\n"
 defaults write com.apple.dock mru-spaces -bool true
 
-# Remove the auto-hiding Dock delay
+printf "Remove the auto-hiding Dock delay\n"
 defaults write com.apple.dock autohide-delay -float 0
 
-# Speed up animation when hiding/showing the Dock
+printf "Speed up animation when hiding/showing the Dock\n"
 defaults write com.apple.dock autohide-time-modifier -float 0.3
 
-# Dock: automatically hide and show
+printf "Dock: automatically hide and show\n"
 defaults write com.apple.dock autohide -bool true
 
-# Dock: make icons of hidden applications translucent
+printf "Dock: make icons of hidden applications translucent\n"
 defaults write com.apple.dock showhidden -bool false
 
-# Screen
+printf "\n\nScreen\n\n"
 
-# Require password immediately after sleep or screen saver begins
+printf "Require password immediately after sleep or screen saver begins\n"
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
-# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+printf "Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)\n"
 defaults write com.apple.screencapture type -string "png"
 
-# Enable subpixel font rendering on non-Apple LCDs
+printf "Enable subpixel font rendering on non-Apple LCDs\n"
 defaults write NSGlobalDomain AppleFontSmoothing -int 2
 
-# Finder
+printf "\n\nFinder\n\n"
 
-# Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
+printf "Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons\n"
 defaults write com.apple.finder QuitMenuItem -bool true
 
-# Finder: disable window and Get Info animations
+printf "Finder: disable window and Get Info animations\n"
 defaults write com.apple.finder DisableAllAnimations -bool true
 
-# General: Enable icons for hard drives, servers, and removable media on the desktop
+printf "General: Enable icons for hard drives, servers, and removable media on the desktop\n"
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
-# Finder: show hidden files by default
+printf "Finder: show hidden files by default\n"
 defaults write com.apple.finder AppleShowAllFiles -bool true
 
-# Finder: show all filename extensions
+printf "Finder: show all filename extensions\n"
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-# Finder: show status bar
+printf "Finder: show status bar\n"
 defaults write com.apple.finder ShowStatusBar -bool true
 
-# Finder: show path bar
+printf "Finder: show path bar\n"
 defaults write com.apple.finder ShowPathbar -bool true
 
-# Finder: allow text selection in Quick Look
+printf "Finder: allow text selection in Quick Look\n"
 defaults write com.apple.finder QLEnableTextSelection -bool true
 
-# Finder: display full path as Finder window title
+printf "Finder: display full path as Finder window title\n"
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
-# When performing a search, search the current folder by default
+printf "When performing a search, search the current folder by default\n"
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
-# General: enable the warning when changing a file extension
+printf "General: enable the warning when changing a file extension\n"
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool true
 
-# Finder: enable spring loading for directories
+printf "Finder: enable spring loading for directories\n"
 defaults write NSGlobalDomain com.apple.springing.enabled -bool false
 
-# Remove the spring loading delay for directories
+printf "Remove the spring loading delay for directories\n"
 defaults write NSGlobalDomain com.apple.springing.delay -float 0
 
-# General: create .DS_Store files on network volumes
+printf "General: create .DS_Store files on network volumes\n"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool false
 
-# Finder: automatically open a new window when a volume is mounted
+printf "Finder: automatically open a new window when a volume is mounted\n"
 defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
 defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
 defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 
-# Use columns view in all Finder windows by default
-# Four-letter codes for the other view modes: `icnv`, `Nlsv`, `Flwv`
+printf "Use columns view in all Finder windows by default\n"
+printf "Four-letter codes for the other view modes: `icnv`, `Nlsv`, `Flwv`\n"
 defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
 
-# General: enable the warning before emptying the Trash
+printf "General: enable the warning before emptying the Trash\n"
 defaults write com.apple.finder WarnOnEmptyTrash -bool true
 
-# Finder: enable AirDrop over Ethernet and on unsupported Macs running Lion
+printf "Finder: enable AirDrop over Ethernet and on unsupported Macs running Lion\n"
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
 
-# Show the ~/Library folder
+printf "Show the ~/Library folder\n"
 chflags nohidden ~/Library
+
+printf "################ 💩 DONE 💩 ################\n\n"
